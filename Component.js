4414f014966dcbac354e05bf4e4dc220fc404fe7
Olena Zdanovska <freshjs@gmail.com>
2023-08-23 09:28:55 +0300

    Add commonJS module

---
+++ Component.js
@@ -0,0 +1,5 @@
+class Component {
+  render() {
+    console.log('Component render');
+  }
+}
---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+const {MyMath} = require('./MyMath');
+console.log('hi, node!');
+//console.log(MyMath);
+const result = MyMath.mult(2, 4);
+console.log('result = ', result);
+
+// const component = new Component();
+// component.render();
